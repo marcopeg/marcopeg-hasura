8ddd223cebb4a9961e4bfb9e0248bc14d781e8f9
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-12-21 11:15:02 +0000

    Wip on Auth0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from './lib/auth0';
 
-function App() {
+const App = () => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0;
+  console.log(isAuthenticated, loginWithRedirect)
+
+  if (isAuthenticated) {
+    return 'you are logged in'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div onClick={loginWithRedirect}>login!</div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Auth0Provider } from './lib/auth0';
+import history from './lib/history';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={'marcopeg.eu.auth0.com'}
+    client_id={'1dFca4fBVhVYhMiF9K96EL6lsfmqRgD6'}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
