1078c7784749fbfbc9795e8450986ae24a158ffa
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-01-04 10:14:45 +0100

    Revert open modal

--- services/frontend/src/views/Dashboard.jsx
+++ services/frontend/src/views/Dashboard.jsx
@@ -17,7 +17,7 @@ import { withAuth } from '../lib/auth';
 import ExpenseEntryModal from '../containers/ExpenseEntryModal';
 
 const Dashboard = ({ auth }) => {
-  const [ showExpenseModal, setShowExpenseModal ] = useState(true)
+  const [ showExpenseModal, setShowExpenseModal ] = useState(false)
 
   return (
     <IonPage>
