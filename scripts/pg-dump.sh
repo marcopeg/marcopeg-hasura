22bf9ed0a8954436205bbe2bbc0ae8f14d420f3c
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-06 09:14:31 +0200

    Add pg-dump script

--- scripts/pg-dump.sh
+++ scripts/pg-dump.sh
@@ -4,7 +4,7 @@ mkdir -p ${HUMBLE_BACKUP}
 
 # ensure parameters
 CONN_STR=${PG_STRING_REMOTE/@localhost:/@$HUMBLE_IP:}
-ABS_PATH=${HUMBLE_BACKUP}/export_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+ABS_PATH=${HUMBLE_BACKUP}/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
 echo "> Run backup to local directory"
 echo ${ABS_PATH}
@@ -13,11 +13,11 @@ echo ""
 mkdir -p ${HUMBLE_DATA}
 mkdir -p ${HUMBLE_BACKUP}
 docker run --rm postgres pg_dump \
+    --schema=public \
     --clean \
     --no-owner \
     --no-privileges \
     --no-acl \
-    -F p \
     --inserts \
     --dbname=${CONN_STR} \
     > ${ABS_PATH}
